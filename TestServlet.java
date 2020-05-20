b0ff71b895109177f5c685d3009bba9eef309958
1Emanuel1 <64265286+1Emanuel1@users.noreply.github.com>
2020-05-20 18:26:31 +0100

    Update TestServlet.java

--- TestServlet.java
+++ TestServlet.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package rg.glassfish.samples.entities;
+package org.glassfish.samples.entities;
 
 import java.io.IOException;
 import java.io.PrintWriter;
